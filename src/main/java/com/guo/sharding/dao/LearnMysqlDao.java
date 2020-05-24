9cade3365e016a265bed56c02b72d924f88028d7
hotlove_linx <guolejian@126.com>
2020-05-24 15:49:47 +0800

    first commit

---
+++ src/main/java/com/guo/sharding/dao/LearnMysqlDao.java
@@ -0,0 +1,18 @@
+package com.guo.sharding.dao;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Auther: hotlove_linx
+ * @Date: 2020/5/24 15:25
+ * @Description:
+ */
+@Mapper
+@Component
+public interface LearnMysqlDao {
+    @Insert("insert into s1(key1, key2, key3, key_part1, key_part2, key_part3, common_field) values (#{k1}, #{k2}, #{k3}, #{key_part1}, #{key_part2}, #{key_part3}, #{common_field})")
+    int insertk(@Param("k1") String k1, @Param("k2") Integer k2, @Param("k3") String k3, @Param("key_part1") String key_part1, @Param("key_part2") String key_part2, @Param("key_part3") String key_part3, @Param("common_field") String common_field);
+}
--- src/test/java/com/guo/sharding/dao/OrderDaoTest.java
+++ src/test/java/com/guo/sharding/dao/OrderDaoTest.java
@@ -11,6 +11,7 @@ import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * @Auther: hotlove_linx
@@ -24,6 +25,9 @@ public class OrderDaoTest {
     @Resource
     OrderDao orderDao;
 
+    @Resource
+    LearnMysqlDao learnMysqlDao;
+
     @Test
     public void testInsertOrder() {
         for (int i = 1; i < 20; i++) {
@@ -35,5 +39,24 @@ public class OrderDaoTest {
     public void testSelectOrder() {
         List<Map> maps = orderDao.selectOrderByIds(Arrays.asList(461651844507107328L, 461651844704239617L));
         System.out.println(maps);
+    }
+
+    @Test
+    public void testLearnMysql() {
+        String [] str = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "l", "m", "n", "o", "p", "q", "r", "t",
+                "A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "T"};
+
+        Random random = new Random();
+
+        for (int i = 0; i < 10000; i++) {
+            int k1 = random.nextInt(36);
+//            int k2 = random.nextInt(36);
+            int k3 = random.nextInt(36);
+            int kp1 = random.nextInt(36);
+            int kp2 = random.nextInt(36);
+            int kp3 = random.nextInt(36);
+            int kc = random.nextInt(36);
+            learnMysqlDao.insertk(str[k1], i, str[k3], str[kp1], str[kp2],str[kp3],str[kc]);
+        }
     }
 }
